4f133f0ff9b45eef002320a6be08168bb4f2d0e2
tonildo123 <tony81191@gmail.com>
2023-10-10 19:19:29 -0300

    update cards post

--- src/components/CardPosts.js
+++ src/components/CardPosts.js
@@ -13,10 +13,23 @@ const CardPosts = ({ lastPost, posts }) => {
 
   const truncateText = (text, maxLength) => {
     if (text.length > maxLength) {
-      return text.substring(0, maxLength) + '...';
+      return (
+        <span>
+          {text.substring(0, maxLength)}
+          <span style={{ color: 'blue' }}>{`... ver más >>`}</span>
+        </span>
+      );
     }
     return text;
   }
+
+  const truncateTextTitle = (text, maxLength) => {
+    if (text.length > maxLength) {
+      return ( text.substring(0, maxLength) + '...');
+    }
+    return text;
+  }
+
 
   return (
     <Grid container spacing={3}>
@@ -28,19 +41,19 @@ const CardPosts = ({ lastPost, posts }) => {
                 alt="Imagen 1"
                 style={{ width: '100%', maxHeight: '70%', objectFit: 'contain' }}
               />
-              <CardContent sx={{ display: 'flex', justifyContent: 'space-around', px: 8 }}>
-                <Typography variant="h6" gutterBottom sx={{ width: '25%', height: '100%' }}>
-                  {truncateText(lastPost.Titulo)}
+              <CardContent sx={{ display: 'flex', justifyContent: 'space-around', px: 1 }}>
+                <Typography  sx={{ width: '25%', height: '100%', fontWeight:'bold',textAlign: 'left' }}>
+                  {truncateTextTitle(lastPost.Titulo, 40)}
                 </Typography>
-                <Typography variant="body2" color="textSecondary" sx={{ width: '75%', height: '100%', px: 2 }}>
-                  {truncateText(lastPost.Detail, 20)}
+                <Typography  sx={{ width: '75%', height: '100%', px: 2,textAlign: 'left' }}>
+                  {truncateText(lastPost.Detail, 180)}
                 </Typography>
               </CardContent>
             </Card>
           </Link>        
       </Grid>
       <Grid item xs={12} sm={6}>
-        {posts.slice(0, 3).map((postItem) => (
+        {posts.slice(1, 4).map((postItem) => (
           <Link to={`/cards/detail`} state={{ card: postItem }} key={postItem.id} style={{ textDecoration: 'none' }}>
             <Card sx={{ display: 'flex', height: { xs: 100, sm: 150 } }}>
               <img
@@ -48,20 +61,13 @@ const CardPosts = ({ lastPost, posts }) => {
                 alt="Imagen 1"
                 style={{ width: '30%', objectFit: 'contain' }}
               />
-              <CardContent sx={{ flex: 'none' }}>
-                <Box sx={{ display: 'flex', width: '100%', justifyContent: 'space-around' }}>
-                  <Typography component="h2" variant="h5" sx={{ mx:1 }}>
-                    {truncateText(postItem.Titulo, 20)}
+              <CardContent sx={{ display: 'flex', justifyContent: 'space-around', px: 1 }}>                
+                  <Typography sx={{ width: '25%', height: '100%', fontWeight:'bold', textAlign: 'left' }}>
+                    {truncateTextTitle(postItem.Titulo, 40)}
                   </Typography>
-                  <Typography variant="subtitle1" paragraph sx={{ mx:1 }}>
-                    {truncateText(postItem.Detail, 20)}
-                  </Typography>
-                </Box>
-                <Box sx={{ display: 'flex', width: '100%', justifyContent: 'flex-end', alignItems:'flex-end' }}>
-                  <Typography variant="subtitle1" color="primary">
-                    {`>>`} ver mas
-                  </Typography>
-                </Box>
+                  <Typography  sx={{ width: '75%', height: '100%', px: 2, textAlign: 'left' }}>
+                    {truncateText(postItem.Detail, 100)}
+                  </Typography>              
               </CardContent>
             </Card>
           </Link>
--- src/pages/posts/allPosts.js
+++ src/pages/posts/allPosts.js
@@ -6,25 +6,44 @@ const AllPosts = () => {
   let { state } = useLocation();
   console.log('all post', state)
 
+  const truncateText = (text, maxLength) => {
+    if (text.length > maxLength) {
+      return (
+        <span>
+          {text.substring(0, maxLength)}
+          <span style={{ color: 'blue' }}>{`... ver más >>`}</span>
+        </span>
+      );
+    }
+    return text;
+  }
+
+  const truncateTextTitle = (text, maxLength) => {
+    if (text.length > maxLength) {
+      return ( text.substring(0, maxLength) + '...');
+    }
+    return text;
+  }
+
   return (    
       <Grid container spacing={3} sx={{my:'2%', px:'5%'}}>
         {state.card.map((postItem) => (
           <Grid item xs={12} sm={4} key={postItem.id}>
-            <Link to={`/cards/detail`} state={{ card: postItem }}>
+            <Link to={`/cards/detail`} state={{ card: postItem }} style={{ textDecoration: 'none' }}>
               <Card sx={{ display: 'flex', height: 200, border: '1px solid #EBF5FB' }}>
                 <img
                   src={postItem.Photo}
                   alt="Imagen 1"
                   style={{ width: '30%', objectFit: 'contain' }}
                 />
-                <CardContent sx={{ flex: 1 }}>
-                  <Typography component="h2" variant="h5">
-                    {postItem.Titulo}
+                <CardContent sx={{ display: 'flex',flexDirection:'column', px: 1 }}>                
+                  <Typography sx={{ width: '100%',fontWeight:'bold', textAlign: 'center' }}>
+                    {truncateTextTitle(postItem.Titulo, 40)}
                   </Typography>
-                  <Typography variant="subtitle1" paragraph>
-                    {postItem.Detail}
-                  </Typography>
-                </CardContent>
+                  <Typography  sx={{ width: '100%', px: 2, textAlign: 'left' }}>
+                    {truncateText(postItem.Detail, 100)}
+                  </Typography>              
+              </CardContent>
               </Card>
             </Link>
           </Grid>
