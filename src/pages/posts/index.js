766dd28bd6b192f66002d9d7932fecacbfa13725
tonildo123 <tony81191@gmail.com>
2023-10-12 15:16:40 -0300

    links matechaebles

--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
 import { Card, CardContent, Typography, Box } from '@mui/material';
 import { useLocation } from 'react-router-dom';
+
 import './style.css'
 
 
 const ResponsiveCard = (card) => {
 
     let { state } = useLocation();
-    console.log('card', state)
-    
+    const renderLinksInText = (text) => {
+        const regex = /https?:\/\/[^\s]+|www\.[^\s]+/g;
+        const parts = text.split(regex);
+        const matches = text.match(regex);
+      
+        return parts.map((part, index) => (
+          <div key={index}>
+            {part}
+            {matches && matches[index] && (
+              <a
+                href={matches[index].startsWith('http') ? matches[index] : `http://${matches[index]}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {matches[index]}
+              </a>
+            )}
+          </div>
+        ));
+      };
+      
 
     return (
         <Box mt={4} className="centered-card">
@@ -23,7 +42,7 @@ const ResponsiveCard = (card) => {
                         {state.card.Titulo}
                     </Typography>
                     <Typography variant="body2" color="textSecondary">
-                        {state.card.Detail}
+                        {renderLinksInText(state.card.Detail)}
                     </Typography>
                 </CardContent>
             </Card>
